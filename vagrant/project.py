fe83e7b2cad6cd09ee3c0c3b736feb837be91d9a
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2023-08-26 13:32:13 -0500

    decorators for crud in routes

--- vagrant/project.py
+++ vagrant/project.py
@@ -5,24 +5,40 @@ from database_setup import Restaurant, MenuItem, Base
 
 app = Flask(__name__)
 
-
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def hello_world():  # Changed function name to follow Flask naming convention
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(id = restaurant.id)
-    message= ''
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurant_menu(restaurant_id=None):  # Make the parameter optional with default None
+    if restaurant_id is None:
+        return "Welcome to the restaurant menu!"
+    
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    message = ''
     for item in items:
         message += item.name
         message += '<br>'
-        return message
-    
+    return message
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
